7346750f12425d73a4034d251c2703985b57984c
robbie <franc.mer2022@gmail.com>
2026-04-30 08:49:27 +0800

    jinja-fy event page and login page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import flask_app
 from flask import render_template
 
+@flask_app.route("/login")
+def login():
+    return render_template("login.html.jinja")
+
 @flask_app.route("/")
-def login_page():
+def home_page():
     return render_template("home.html.jinja")
+
+@flask_app.route("/create_event")
+def event_page():
+    return render_template("create_event.html.jinja")
