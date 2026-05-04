53b21eb9ba6ba2475044b7bc2fc4391e16c714a1
Md. Irtiza Anam <anamirtiza@gmail.com>
2026-05-04 11:27:16 +0800

    Added login route in routes file

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,13 @@ from app import flask_app
 from flask import render_template
 
 @flask_app.route("/")
-def login_page():
+def home_page():
     return render_template("home.html")
 
 @flask_app.route("/register")
 def register():
     return render_template("register.html")
+
+@flask_app.route("/")
+def login_page():
+    return render_template("login.html")    
