7130eb6abbc2016e9e48f92948c61d2ee7976253
tallerasian <rayhao.han@gmail.com>
2026-05-07 17:17:52 +0800

    Enhance event management: add CSRF protection, update event card links, and improve join event form handling
    - home.html now link o event/id/heatmap
    - join tab replaced with real event join form with CSRF token
    - remove fake join code ()
    - controls whether user sees next invite or done
    - sytle sheet updates to event card link

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
 from app.config import Config
 
 flask_app = Flask(__name__)
@@ -9,6 +10,7 @@ flask_app.config.from_object(Config)
 
 db = SQLAlchemy(flask_app)
 bcrypt = Bcrypt(flask_app)
+csrf = CSRFProtect(flask_app)  # makes csrf_token() available in all templates
 login_manager = LoginManager(flask_app)
 login_manager.login_view = "login_page"
 
