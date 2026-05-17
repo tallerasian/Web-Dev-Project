79726fb037ac4ae9981a1b153f22e1a8fe963e61
robbie <franc.mer2022@gmail.com>
2026-05-17 19:03:27 +0800

    password encryption unittest

--- testing/unittests.py
+++ testing/unittests.py
@@ -1,5 +1,7 @@
 from app import create_app, db
 from app.config import TestConfig
+from app.functionality import add_event, add_user
+from app.models import User
 from unittest import TestCase
 
 class BasicTests(TestCase):
@@ -9,9 +11,27 @@ class BasicTests(TestCase):
         self.app_context.push()
 
         # perform additional setup steps here (e.g. adding data to the database)
-    
-    def test_unittest_is_working(self):
-        print("Hello World")
+
+    def test_password_encryption(self):
+        """Test password encryption"""
+
+        user = User()
+        password = "password123"
+        user.set_password(password)
+        encrypted = user.password_hash
+
+        self.assertNotEqual(password, encrypted, "Password has not been encrypted")
+        self.assertTrue(user.check_password(password), "Checking encrypted password has failed")
+
+        new_user = User()
+        new_password = "password123"
+        new_user.set_password(new_password)
+        new_encrypted = new_user.password_hash
+
+        self.assertNotEqual(encrypted, new_encrypted, "Password salting has failed")
+
+
+        
     
     def tearDown(self):
         db.session.remove()
